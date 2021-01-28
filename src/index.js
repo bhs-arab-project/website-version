621170a1057ef42b933fde3a011b4d63544840c0
Khafidh Fuadi <muhammadkhafidfuadi@gmail.com>
2021-01-28 12:09:44 +0700

    rapihin files next

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ import "assets/scss/now-ui-kit.scss?v=1.4.0";
 import "assets/demo/demo.css?v=1.4.0";
 import "assets/demo/nucleo-icons-page-styles.css?v=1.4.0";
 // pages for this kit
-import Home from "template/home.js";
+import Home from "./home.js";
 import NucleoIcons from "template/NucleoIcons.js";
 import LoginPage from "auth/LoginPage";
 import BabPage from "./user/murid/Bab-page";
