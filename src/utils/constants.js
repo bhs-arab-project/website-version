583fa713a3e8da0ac758edacfabc80b32918f8ec
Khafidh Fuadi <muhammadkhafidfuadi@gmail.com>
2021-03-25 09:46:34 +0700

    ss

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 // export const API_URL = "http://10.0.2.198:8000/api/";
-export const API_URL = "http://base-data.herokuapp.com/api/";
+export const API_URL = "https://base-data.herokuapp.com/api/";
