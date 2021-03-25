a424bf927da6a3db4b5ad605e07dde409514035b
Khafidh Fuadi <muhammadkhafidfuadi@gmail.com>
2021-03-25 09:32:51 +0700

    Updates

---
+++ precache-manifest.b0b4643902cf15be553df045053fb301.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fef4127016a3d96e6717949fa94ee3",
+    "url": "/website-version/index.html"
+  },
+  {
+    "revision": "455c9991caf429bfcfd5",
+    "url": "/website-version/static/css/2.ceba968d.chunk.css"
+  },
+  {
+    "revision": "fd8084a6183dfabbb62d",
+    "url": "/website-version/static/css/main.d6be3a2a.chunk.css"
+  },
+  {
+    "revision": "455c9991caf429bfcfd5",
+    "url": "/website-version/static/js/2.c114c0d3.chunk.js"
+  },
+  {
+    "revision": "9787f6b2abfc0aa35c183f4b787c4227",
+    "url": "/website-version/static/js/2.c114c0d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd8084a6183dfabbb62d",
+    "url": "/website-version/static/js/main.50eeb8e6.chunk.js"
+  },
+  {
+    "revision": "44addedc1708e257ec2a",
+    "url": "/website-version/static/js/runtime-main.08586c08.js"
+  },
+  {
+    "revision": "0f180ac62339ddab701f0b690c2923ee",
+    "url": "/website-version/static/media/bab-detail-bg.0f180ac6.png"
+  },
+  {
+    "revision": "d33fe60098938f45537fc923efe17136",
+    "url": "/website-version/static/media/bab-header.d33fe600.jpg"
+  },
+  {
+    "revision": "9f9ba1644ceb9ff4f081f619e152108f",
+    "url": "/website-version/static/media/book2.9f9ba164.png"
+  },
+  {
+    "revision": "cfe7f4d8749cabdb67ad64884c047a4c",
+    "url": "/website-version/static/media/books.cfe7f4d8.png"
+  },
+  {
+    "revision": "e0126a684951c50a1ae3aff520125a73",
+    "url": "/website-version/static/media/cert-icon.e0126a68.png"
+  },
+  {
+    "revision": "47ba7c4c5b979d11d56422e3fda68027",
+    "url": "/website-version/static/media/class.47ba7c4c.jpg"
+  },
+  {
+    "revision": "f809209308af04d9d1168314cb087661",
+    "url": "/website-version/static/media/header2.f8092093.jpg"
+  },
+  {
+    "revision": "bc2f0f40bb265c7abcb98428d3b51ecc",
+    "url": "/website-version/static/media/muslim.bc2f0f40.png"
+  },
+  {
+    "revision": "fcab4841ff0907b7d97bee97060681cf",
+    "url": "/website-version/static/media/my-bab.fcab4841.jpg"
+  },
+  {
+    "revision": "55cd6690f4a7a15d1c84c98f3e973a92",
+    "url": "/website-version/static/media/my-babex.55cd6690.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/website-version/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/website-version/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/website-version/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/website-version/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "7e0ee156e433f35da572b1ef336a1bfe",
+    "url": "/website-version/static/media/pnf.7e0ee156.png"
+  },
+  {
+    "revision": "454f3d4dc7d1011e6699c94c5fdc79ef",
+    "url": "/website-version/static/media/quizlist.454f3d4d.jpg"
+  },
+  {
+    "revision": "ca4cc7885220428bca866b047f9ad59f",
+    "url": "/website-version/static/media/suc3.ca4cc788.png"
+  },
+  {
+    "revision": "a81af10217d0970f6c9d92db6fe9c88a",
+    "url": "/website-version/static/media/try-again.a81af102.jpg"
+  }
+]);
